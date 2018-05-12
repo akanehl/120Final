6384cbf3b4276ff089d5c3c6a7ad6818bc369643
Aaron Kanehl <akanehl@ucsc.edu>
2018-05-12 16:41:04 -0700

    Added my Assets Folder

---
+++ assets/img/Tile Sets/Ice Physics Walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Ice-Physics-Walls" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../.PNG format/Ice-Physics-Walls.png" width="128" height="128"/>
+</tileset>
